d25bad61041928a2787e04acf1ec404d0f99146c
Esha Pakalapati <151759711+RainyAlgorithms@users.noreply.github.com>
2025-07-30 10:44:42 -0400

    Update users.py

--- backend/routers/users.py
+++ backend/routers/users.py
@@ -5,3 +5,7 @@ router = APIRouter(prefix="/users", tags=["Users"])
 @router.post("/")
 def create_user():
     return {"message": "User creation stub"}
+
+@router.get("/")
+def get_user():
+    return {"message": "Get requested User"}
