41dc86e3d466112d3ee2ec2813469e5540bfbf6b
Esha Pakalapati <151759711+RainyAlgorithms@users.noreply.github.com>
2025-07-30 10:48:18 -0400

    Update matches.py
    creating get_match

--- backend/routers/matches.py
+++ backend/routers/matches.py
@@ -5,3 +5,7 @@ router = APIRouter(prefix="/matches", tags=["Matches"])
 @router.post("/")
 def create_match():
     return {"message": "Match creation stub"}
+
+@router.get("/")
+def get_match():
+    return {"message": "Get requested Match"}
