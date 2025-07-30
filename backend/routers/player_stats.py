97653123d42b1ed7c6bc6595b95b428643cfd127
Esha Pakalapati <151759711+RainyAlgorithms@users.noreply.github.com>
2025-07-30 14:10:39 -0400

    Update player_stats.py
    fixed

--- backend/routers/player_stats.py
+++ backend/routers/player_stats.py
@@ -2,6 +2,6 @@ from fastapi import APIRouter
 
 router = APIRouter(prefix="/users", tags=["Player Stats"])
 
-@router.get("/")
+@router.get("/users")
 def get_player_stats():
     return {"message": "Get requested player's stats"}
