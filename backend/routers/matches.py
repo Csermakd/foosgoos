9d13364d75461e4be5a53c894c8f5073ed274cce
Esha Sri Pakalapati <esha.pakalapati@mail.utoronto.ca>
2025-08-04 18:54:49 -0400

    #22: Adding backend logic to endpoints

--- backend/routers/matches.py
+++ backend/routers/matches.py
@@ -1,11 +1,31 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Depends, HTTPException
+from sqlalchemy.orm import Session
+from models.match import Match as MatchModel
+from schemas.match_schema import MatchCreate, Match
+from database import get_db
+from typing import List
 
 router = APIRouter(prefix="/matches", tags=["Matches"])
 
+
 @router.post("/")
-def create_match():
-    return {"message": "Match creation stub"}
+def create_match(match: MatchCreate, db: Session = Depends(get_db)):
+    db_match = MatchModel(
+        player1_id=match.player1_id,
+        player2_id=match.player2_id,
+        player3_id=match.player3_id,
+        player4_id=match.player4_id,
+        winner_team=match.winner_team
+    )
+    db.add(db_match)
+    db.commit()
+    db.refresh(db_match)
+    return db_match
+
 
 @router.get("/")
-def get_match():
-    return {"message": "Get requested Match"}
+def get_match(match_id: int, db: Session = Depends(get_db)):
+    match = db.query(MatchModel).filter(MatchModel.id == match_id).first()
+    if not match:
+        raise HTTPException(status_code=404, detail="Match not found")
+    return match
