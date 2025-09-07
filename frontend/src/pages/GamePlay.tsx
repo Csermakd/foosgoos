92b1ea233acaf17665ba2e1ef2fe1f92f7607079
Samudra Perera <samudrapup@gmail.com>
2025-09-07 01:07:14 -0400

    Redux store works

--- frontend/src/pages/GamePlay.tsx
+++ frontend/src/pages/GamePlay.tsx
@@ -1,9 +1,17 @@
+import {useSelector} from "react-redux";
+import { type RootState } from "@/store";
+
 type GamePlayPageProps = {
     //teamA: Team;
     //teamB: Team;
 }
 
 const GamePlay = (props: GamePlayPageProps) => {
+    const bluePlayers = useSelector((state: RootState) => state.game.blue);
+    const redPlayers = useSelector((state: RootState) => state.game.red);
+
+    console.log('Blue Team Players:', bluePlayers);
+    console.log('Red Team Players:', redPlayers);
   return (
     <div>Some dummy text</div>
   )
