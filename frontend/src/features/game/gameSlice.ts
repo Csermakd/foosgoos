97348f4b91730bce08adf9418f14113bb2a7d405
Samudra Perera <samudrapup@gmail.com>
2025-10-08 16:59:09 -0400

    Type Correction

--- frontend/src/features/game/gameSlice.ts
+++ frontend/src/features/game/gameSlice.ts
@@ -1,24 +1,25 @@
 import { createSlice, type PayloadAction } from '@reduxjs/toolkit';
+import { type PlayerAssignment } from '@/types/Game';
 
 type TeamPlayers = {
-    blue: string[];
-    red: string[];
+  blue: PlayerAssignment[];
+  red: PlayerAssignment[];
 }
 
 const intialState: TeamPlayers = {
-    blue: [],
-    red: []
+  blue: [],
+  red: []
 }
 
 const gameSlice = createSlice({
-    name: 'game',
-    initialState: intialState,
-    reducers: {
-        setPlayers(state, action: PayloadAction<TeamPlayers>) {
-            state.blue = action.payload.blue;
-            state.red = action.payload.red;
-        },
+  name: 'game',
+  initialState: intialState,
+  reducers: {
+    setPlayers(state, action: PayloadAction<TeamPlayers>) {
+      state.blue = action.payload.blue;
+      state.red = action.payload.red;
     },
+  },
 })
 
 export const { setPlayers } = gameSlice.actions;
